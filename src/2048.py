897ec575842f2feea491e89bcfc6f1464651f656
Nimit Bhardwaj <nimitbhardwaj@gmail.com>
2018-02-04 01:42:07 +0530

    Work with truth

--- src/2048.py
+++ src/2048.py
@@ -1,59 +1,96 @@
 import pygame
 from pygame.locals import *
+import sys
 import random
 from constant import *
-import util
 from color import colors
+import box
+from enum import Enum
 
+MAT = [[box.Box(i, j) for i in range(4)] for j in range(4)]
+M_ABLE = [[False for i in range(4)] for j in range(4)]
+BLOCK = [[False for i in range(4)] for j in range(4)]
 
-'''def move_left():
-	ok = True
-	for i in range(1, 4):
-		for j in range(4):
-			if move[j][i] and move[j - 1][i]:
-				if Mat[j - 1][i] == 0:
-					Mat[j - 1][i] = Mat[j][i]
-					Mat[j][i] = 0
-				else if Mat[j - 1][i] == Mat[j][i] and Mat[j][i] > 0:
-					Mat[j - 1][i] = 2 * Mat[j][i]
-					Mat[j][i] = 0
-					move[j][i] = 0
-			else if Mat[j - 1][i] == 0:
-				Mat[j - 1][i] = Mat[j][i]
-				Mat[j][i] = 0
-	# compare
-
-	return Mat, ok;'''
+class Move(Enum):
+    UP = 1
+    DOWN = 2
+    LEFT = 3
+    RIGHT = 4
+    NULL = 0
 
 def main():
-	SCR = pygame.display.set_mode((WINWIDTH, WINHEIGHT))
-	pygame.display.set_caption('2048 AI')
+    SCR = pygame.display.set_mode((WINWIDTH, WINHEIGHT))
+    pygame.display.set_caption('2048 AI')
 
-	SCR.fill((255, 255, 255))
-	FPSCLOCK = pygame.time.Clock()
-	running = True
-	makeBoard(SCR)
-	while running:
-		for e in pygame.event.get():
-			if e.type == QUIT:
-				running = False
-		makeBoard(SCR)
-		pygame.display.flip()
-		FPSCLOCK.tick(FPS)
-	pygame.exit()
-	sys.exit()
+    FPSCLOCK = pygame.time.Clock()
+    running = True
+    inMotion = False
+    emptyList = []
+    for i in range(4):
+        for j in range(4):
+            if MAT[i][j].val == 0:
+                emptyList.append((i, j))
+    bleedingEffect(emptyList)
+    bleedingEffect(emptyList)
+    mv = Move.NULL
+    makeBoard(SCR)
+    while running:
+        for e in pygame.event.get():
+            if e.type == QUIT:
+                running = False
+            elif e.type == KEYDOWN:
+                if not inMotion and e.key == K_LEFT:
+                    inMotion = True
+                    mv = Move.LEFT
+                    moveLeft()
+                elif not inMotion and e.key == K_RIGHT:
+                    inMotion = True
+                    mv = Move.RIGHT
+                    moveRight()
+                elif not inMotion and e.key == K_UP:
+                    inMotion = True
+                    mv = Move.UP
+                    moveUp()
+                elif not inMotion and e.key == K_DOWN:
+                    inMotion = True
+                    mv = Move.DOWN
+                    move(Down)
+                elif e.key == K_ESCAPE:
+                    running = False
+        makeBoard(SCR)
+        pygame.display.flip()
+        FPSCLOCK.tick(FPS)
+    pygame.quit()
+    sys.exit()
 
 def makeBoard(srf):
-	r1 = pygame.Rect(WINMARGIN - 10, WINMARGIN - 10, BOARDSIDE + 20, BOARDSIDE + 20)
+    r1 = pygame.Rect(WINMARGIN - 10, WINMARGIN - 10, BOARDSIDE + 20, BOARDSIDE + 20)
+    srf.fill((255, 255, 255))
 
-	pygame.draw.rect(srf, colors['antiquewhite1'], r1)
-	for i in range(4):
-		for j in range(4):
-			if Mat[i][j] == 0:
-				r = pygame.Rect(WINMARGIN + BOXSIZE * i + GAPSIZE * i,
-					WINMARGIN + BOXSIZE * j + GAPSIZE * j, BOXSIZE, BOXSIZE)
-				#pygame.draw.rect(srf, colors['antiquewhite3'], r)
-				util.BOX(srf, r, colors['antiquewhite3'], 0.4, 0)
+    pygame.draw.rect(srf, colors['antiquewhite1'], r1)
+    for i in range(4):
+        for j in range(4):
+            box.Box(i, j, 0).draw(srf)
+    for i in range(4):
+        for j in range(4):
+            MAT[i][j].draw(srf)
 
-if __name__ == "__main__":
-	main()
+def moveLeft():
+    pass
+
+def moveRight():
+    pass
+
+def moveUp():
+    pass
+
+def moveDown():
+    pass
+
+def bleedingEffect(lst):
+    kapa = random.choice(lst)
+    MAT[kapa[0]][kapa[1]].val = 4 if random.random() - 0.1 <= 0 else 2
+    lst.remove(kapa)
+
+if __name__ == '__main__':
+    main()
--- src/constant.py
+++ src/constant.py
@@ -1,3 +1,7 @@
+import pygame
+
+pygame.font.init()
+
 BOARDSIDE = 430
 FPS = 30
 WINWIDTH = 800
@@ -5,7 +9,5 @@ WINHEIGHT = 600
 WINMARGIN = 80
 BOXSIZE = 100
 GAPSIZE = 10
-
-Mat = [[0 for i in range(4)] for j in range(4)]
-
-move = [[1 for i in range(4)] for j in range(4)]
+DELTA = 5
+FONT = pygame.font.SysFont('comicsansms', 16)
